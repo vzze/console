d53decd6bac72c0a0121aba9da686685e3435763
vzze <lolo32778@gmail.com>
2023-03-03 19:38:51 +0200

    update scripts

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,1 +1,10 @@
-cmake --build ./build
+$check_dir = Test-Path ./build -PathType Container
+
+if($check_dir -eq $false) {
+    New-Item build -ItemType Directory
+}
+
+Write-Host "`n-------------- CMAKE --------------`n"
+
+Invoke-Expression "cmake -S . -B build -G `"MinGW Makefiles`""
+Invoke-Expression "cmake --build ./build"
